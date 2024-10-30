3a44c347129331e2ac3d5b0fe728b29a091e8e69
sahilKalvium <sahilpreet.singh.s80@kalvium.community>
2024-10-30 23:05:59 +0530

    adding slides

---
+++ script.js
@@ -0,0 +1,7 @@
+var slides = document.getElementsByClassName("slide")
+for (i in slides){
+    // slides[i].style.visibility = 'hidden';
+    
+    console.log(slides[i].className)
+}
+console.log("out")
